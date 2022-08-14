678cfa1423d33af97f94bd4d02aef673300b2c5e
nikhatsultana639 <102848961+nikhatsultana639@users.noreply.github.com>
2022-08-15 00:59:16 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,3 +1,8 @@
+variable "client_secret" {
+}
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -10,4 +15,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = "b5913168-40de-4d42-8e24-716030312cd1"
+  client_id       = "bc9b33ae-9824-4ce8-80b1-f19a43a98b7c"
+  client_secret   = var.client_secret
+  tenant_id       = "359d658c-cdc8-4097-804d-c58d9588de63"
 }
